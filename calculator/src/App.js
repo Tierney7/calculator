2dea7483a0e1f4cfc2a318511e050cad18cb242d
Matthew Tierney <matt@rankfuse.com>
2024-02-06 09:47:34 -0600

    install/plate

---
+++ calculator/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+      </div>
+      )
+}
+
+
+export default App
